9cee70498054965a8fbc876d1ebe89a81105ff24
Anil DS <anilds04@gmail.com>
2025-08-04 22:14:09 +0100

    Instrumentation test added

--- app/src/main/java/com/example/mvvmhiltjetpackdemo/api/ProductsApi.kt
+++ app/src/main/java/com/example/mvvmhiltjetpackdemo/api/ProductsApi.kt
@@ -3,10 +3,13 @@ package com.example.mvvmhiltjetpackdemo.api
 import com.example.mvvmhiltjetpackdemo.model.ProductsItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ProductsApi {
 
     @GET("/products")
     suspend fun getProducts() : Response<List<ProductsItem>>
 
+    @GET("/products/{id}")
+    suspend fun getProductsById(@Path("id") userId : String) : Response<ProductsItem>
 }
